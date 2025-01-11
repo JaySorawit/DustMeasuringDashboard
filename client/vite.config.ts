add9b2364f46018d5bf0e213f1b809f9833aaece
Jay Sorawit <sorawit.mokt@gmail.com>
2025-01-11 22:04:31 +0700

    Update vercel.json
    Update vercel.json
    
    Update vercel.json
    
    Update vite.config.ts
    
    Delete vercel.json
    
    Update vite.config.ts
    
    Update ci-cd.yml
    
    Update ci-cd.yml
    
    Update ci-cd.yml

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
+  build: {
+    outDir: 'dist',
   },
 });
