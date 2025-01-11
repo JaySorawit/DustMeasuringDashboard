1dea496256d98a2ca401766fa7554fa7ef0b5c13
Jay Sorawit <sorawit.mokt@gmail.com>
2025-01-12 01:02:30 +0700

    Update apiConfig.ts

--- client/src/configs/apiConfig.ts
+++ client/src/configs/apiConfig.ts
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000' || 'http://52.64.110.28:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://52.64.110.28:5000';
+
+console.log('API_BASE_URL:', API_BASE_URL);
 
 export default API_BASE_URL;
