6b597a93296380e3a9692b79a44d42f50a4047c7
Jay Sorawit <sorawit.mokt@gmail.com>
2025-01-22 22:10:12 +0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor';
-            if (id.includes('chart.js')) return 'chart-vendor';
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 3000,
-  },
   plugins: [react()],
   base: '/',
   server: {
