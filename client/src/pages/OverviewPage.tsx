604d5ab20a173be628f8fb5b4ed8e2e3dd82195a
Jay Sorawit <114494431+JaySorawit@users.noreply.github.com>
2025-04-04 22:50:23 +0700

    Reset overview page

--- client/src/pages/OverviewPage.tsx
+++ client/src/pages/OverviewPage.tsx
@@ -1,33 +1,10 @@
-import DustMeasureCount from '../components/DustMeasureCount';
-import DustExceedLoacation from '../components/DustExceedLoacation';
-import DustExceedType from '../components/DustExceedType';
-import LineChart from '../components/LineChart';
-import PieChart from '../components/PieChart';
-import TopLocation from '../components/TopLocation';
+import React from "react";
 
 export default function OverviewPage() {
 
     return (
         <>
             <h1>Overview Page</h1>
-            <div style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
-                <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                    <div style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
-                        <DustMeasureCount />
-                        <DustExceedLoacation />
-                        <DustExceedType />
-                    </div>
-                    <LineChart />
-                </div>
-                <div style={{ display: 'flex', flexDirection:'column', gap: '2rem', padding: '1rem' }}>
-                    <div style={{ display: 'flex', justifyContent: 'center'}}>
-                        <PieChart />
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <TopLocation />
-                    </div>
-                </div>
-            </div>
         </>
     );
 }
